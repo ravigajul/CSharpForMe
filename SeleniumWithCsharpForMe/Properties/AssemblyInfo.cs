2eace9295644d77966e27ad33bf27453077c0ad9
Ravi Gajul <Ravi.Gajul@yahoo.com>
2022-11-28 19:49:59 +0530

    commiting first

---
+++ SeleniumWithCsharpForMe/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumWithCsharpForMe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lenovo")]
+[assembly: AssemblyProduct("SeleniumWithCsharpForMe")]
+[assembly: AssemblyCopyright("Copyright © Lenovo 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fee3e452-ba0d-4ff1-9e62-49b7170de640")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumWithCsharpForMe/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumWithCsharpForMe
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine(ConfigurationSettings.AppSettings.Get("Browser"));
+            Console.WriteLine(ConfigurationSettings.AppSettings.Get("UserName"));
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://formy-project.herokuapp.com/datepicker");
+        }
+    }
+}
